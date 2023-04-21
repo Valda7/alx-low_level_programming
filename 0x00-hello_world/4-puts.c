a0f0f6b8474066cd7872b531b6125fbe6a88b923
Valda7 <valdarian7@gmail.com>
2023-04-20 18:15:41 -0700

    Print a file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+task 4 #include <stdio.h>
+/**
+ * main - You write anything without special keys otherwise betty wont run it
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
