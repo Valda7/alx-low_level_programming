238652ec2afe88d4b64e97020bcfd60cd51eab43
Valda7 <valdarian7@gmail.com>
2023-04-21 19:15:30 -0700

    if

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 int alphabet;
-for (alphabet = 'a'; alphabet < = 'z'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 alphabet = tolower(alphabet);
 putchar(alphabet);
 }
 putchar('\n');
-return (0);
+return(0);
 }
