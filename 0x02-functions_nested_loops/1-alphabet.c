d7be1915d147f857a244d8a5e4c12960a7a4aa2a
Valda7 <valdarian7@gmail.com>
2023-04-25 16:58:06 -0700

    Aplhabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * Print alphabet in lowercase
+ * followed by a new line
+ * Return : Always (0) success
+*/
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
