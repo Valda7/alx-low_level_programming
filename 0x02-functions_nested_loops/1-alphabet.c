cb47badcdd265a3f0070bc59f89fc5e8578e4c37
Valda7 <valdarian7@gmail.com>
2023-04-25 17:43:38 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print alphabet in lowercase
+ * Description: Print alphabet in lowercase
  * followed by a new line
  * Return : Always (0) success
 */
@@ -9,6 +9,7 @@
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
